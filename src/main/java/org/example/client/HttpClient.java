d684786d3a38b9a1497de6fc34c31b72e8710899
Navapol1234 <navapol.s@kkumail.com>
2023-05-22 13:44:51 +0700

    add http-server

--- src/main/java/org/example/client/HttpClient.java
+++ src/main/java/org/example/client/HttpClient.java
@@ -1,6 +1,7 @@
 package org.example.client;
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -9,11 +10,14 @@ import java.io.OutputStreamWriter;
 
 public class HttpClient {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("127.0.0.1", 8080);  //localhost(loopback address)ip
+        Socket socket = new Socket();  //localhost(loopback address)ip
+        socket.connect(new InetSocketAddress("192.168.31.197", 8080));
         BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         BufferedWriter outputStreamWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        outputStreamWriter.write("GET / HTTP/2.1\r\n");
+        outputStreamWriter.write("GET http://localhost:8080 HOHO");
         outputStreamWriter.newLine();
+//        outputStreamWriter.write("Host: localhost\r\n");
+//        outputStreamWriter.newLine();
         outputStreamWriter.flush();
         String line = inputStreamReader.readLine();
         System.out.println(line);
--- src/main/java/org/example/server/HttpServer.java
+++ src/main/java/org/example/server/HttpServer.java
@@ -16,27 +16,29 @@ public class HttpServer {
                 try {
                     System.out.println("aacepted 1 client");
                     BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    BufferedWriter outputStreamWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    BufferedWriter outputStreamWriter1 = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
                     String line = inputStreamReader.readLine();
                     String method = line.split(" ")[0];
                     String resourceUrl = line.split(" ")[1];
+                    String word = line.split(" ")[2];
                     System.out.println("method: " + method);
                     System.out.println("resourceUrl: " + resourceUrl);
+                    System.out.println("wirding: " + word);
                     //response back to client
 
 
                     //servlet side
-                    outputStreamWriter.write("HTTP/1.1 200");
-                    outputStreamWriter.newLine();
-                    outputStreamWriter.newLine();
+                    outputStreamWriter1.write("HTTP/1.1 400");
+                    outputStreamWriter1.newLine();
+                    outputStreamWriter1.newLine();
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    outputStreamWriter.write("<html><body><h1>Hello World</h1></body></html>");
+                    outputStreamWriter1.write("<html><body><h1>Hell Yeah</h1></body></html>");
                     //outputStreamWriter.write(line + " : " + LocalDate.now() );
-                    outputStreamWriter.flush();
+                    outputStreamWriter1.flush();
                     socket.close();
                 } catch (IOException e) {
                     e.printStackTrace();
